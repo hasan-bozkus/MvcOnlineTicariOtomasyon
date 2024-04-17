34d8b8d66378545154df7539b4893426577636d3
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2024-04-17 23:08:06 +0300

    MvcOnlineTicariOtomasyon Projesi
    Bölüm 1 - Giriş:
    Giriş,
    Tablolarımız,
    Model View Controller
    
    Bölüm 2 - Projenin Oluşturulması:
    Projeyi Oluşturma,
    MVC Mimarisine Genel Bakış,
    Proje Ücretsiz Tema Linkleri
    
    Bölüm 3 - Sınıfların Oluşturulması:
    Kategori ve Ürün Sınıfı,
    Cari ve Presonel Sınıfı,
    Fatura ve Departman Sınıfı,
    Admin & Fatura Kalem ve Giderler Sınıfı,
    Satış Hareketleri Sınıfı,
    Kategori ve Ürün İlişkileri,
    Fatura ve Fatura Kalem İlişkileri,
    Satış İlişkileri,
    Personel ve Departman İlişkileri,
    Kısıtlamalar ve Veri Türleri

---
+++ MvcOnlineTicariOtomasyon/Models/Siniflar/Cariler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyon.Models.Siniflar
+{
+    public class Cariler
+    {
+        [Key]
+        public int CariId { get; set; }
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(30)]
+        public string CariAd { get; set; }
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(30)]
+        public string CariSoyad { get; set; }
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(30)]
+        public string CariSehir { get; set; }
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(50)]
+        public string CariMail { get; set; }
+
+        public SatisHareket SatisHareket { get; set; }
+    }
+}
