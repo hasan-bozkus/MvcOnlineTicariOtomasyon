724e20139cd16906826c4a3b33bb3dc8d8e25e84
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2024-04-23 11:26:23 +0300

    MvcOnlineTicariOtomasyon Projesi
    Bölüm 5 - Controller ve View:
    Controller ve View Kavaramları,
    Temayı Projeye Dahil Etme,
    Kategoriler Sayfası ve Tablo Kullanımı,
    Tablo ve Layout Düzenlemeleri,
    Font Awasome İkonları,
    Admin Layout Son Düzenlemeler,
    ToList Metodu ile Kategori Listeleme,
    Kategori Ekleme Controller HttpGet ve HttpPost,
    Html Helper Kaydetme İşlemi,
    Remove Metodu ile Kategori Silme,
    Kategori Bilgilerini Güncelleme Sayfasına Taşıma,
    Kategori Güncelleme Sayfası

---
+++ MvcOnlineTicariOtomasyon/Controllers/KategoriController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Siniflar;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class KategoriController : Controller
+    {
+        Context c = new Context();
+
+        // GET: Kategori
+        public ActionResult Index()
+        {
+            var degerler = c.Kategoris.ToList();
+
+            return View(degerler);
+        }
+
+        [HttpGet]
+        public ActionResult KategoriEkle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult KategoriEkle(Kategori kategori)
+        {
+            c.Kategoris.Add(kategori);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        public ActionResult KategoriSil(int id)
+        {
+            var kategori = c.Kategoris.Find(id);
+            c.Kategoris.Remove(kategori);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult KategoriGetir(int id)
+        {
+            var kategori = c.Kategoris.Find(id);
+            return View("KategoriGetir", kategori);
+        }
+
+        [HttpPost]
+        public ActionResult KategoriGuncelle(Kategori kategori)
+        {
+            var values = c.Kategoris.Find(kategori.KategoriID);
+            values.KategoriAd = kategori.KategoriAd;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
