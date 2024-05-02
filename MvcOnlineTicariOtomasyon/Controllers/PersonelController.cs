4cb6f9965f9d4bd1dbbfc8adb1833f8854665d2a
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2024-05-02 13:31:46 +0300

    MvcOnlineTicariOtomasyon Projesi
    Bölüm 9 - Personel:
    Personel Controller ve Index,
    Yeni Personel Ekleme,
    Personel Detayları

---
+++ MvcOnlineTicariOtomasyon/Controllers/PersonelController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Siniflar;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class PersonelController : Controller
+    {
+        Context c = new Context();
+
+
+        // GET: Personel
+        public ActionResult Index()
+        {
+            var degerler = c.Personels.ToList();
+            return View(degerler);
+        }
+
+        [HttpGet]
+        public ActionResult PersonelEkle()
+        {
+            List<SelectListItem> departman = (from departmans in c.Departmans.ToList()
+                                              select new SelectListItem
+                                              {
+                                                  Text = departmans.DepartmanAd,
+                                                  Value = departmans.DepartmanID.ToString()
+                                              }).ToList();
+            ViewBag.departman = departman;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult PersonelEkle(Personel personel)
+        {
+            c.Personels.Add(personel);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult PersonelGetir(int id)
+        {
+            var personel = c.Personels.Find(id);
+
+
+            List<SelectListItem> departman = (from departmans in c.Departmans.ToList()
+                                              select new SelectListItem
+                                              {
+                                                  Text = departmans.DepartmanAd,
+                                                  Value = departmans.DepartmanID.ToString()
+                                              }).ToList();
+            ViewBag.departman = departman;
+            return View("PersonelGetir", personel);
+        }
+
+        [HttpPost]
+        public ActionResult PersonelGuncelle(Personel personel)
+        {
+            var values = c.Personels.Find(personel.PersonelID);
+            values.PersonelAd = personel.PersonelAd;
+            values.PersonelSoyad = personel.PersonelSoyad;
+            values.PersonelGorsel = personel.PersonelGorsel;
+            values.DepartmanID = personel.DepartmanID;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
