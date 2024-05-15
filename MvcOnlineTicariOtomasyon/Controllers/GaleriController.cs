32b27cf1559c4f46902844f40512b6a590dbf57b
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2024-05-15 21:28:36 +0300

    MvcOnlineTicariOtomasyon Projesi
    Bölüm 13 - Galeri:
    Galeri Temasının Kurulması,
    Ürün Görsellerini Çekme,
    Galeri Sayfası Son Düzenlemeler

---
+++ MvcOnlineTicariOtomasyon/Controllers/GaleriController.cs
@@ -0,0 +1,21 @@
+﻿using MvcOnlineTicariOtomasyon.Models.Siniflar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class GaleriController : Controller
+    {
+        Context c = new Context();
+
+        // GET: Galeri
+        public ActionResult Index()
+        {
+            var values = c.Uruns.ToList();
+            return View(values);
+        }
+    }
+}
