73d1a8b746eb36c4ce2f162bc7e1341aa9b8708c
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2024-05-19 20:18:11 +0300

    MvcOnlineTicariOtomasyon Projesi
    Bölüm 14 - Ürün Detay ve E Ticaret Teması Eklenmesi:
    Ürün Detay Sayfası Temanın Entegre Edilmesi,
    Temayı Düzenleme,
    Tema Linki Düzenlenmiş Hali,
    Ürün Bilgilerini Çekme,
    Detay Sınıfının Eklenmesi,
    Bir View'de Birden Fazla Tablo Listeleme & IEnumerable,
    Genel Bakış

---
+++ MvcOnlineTicariOtomasyon/Controllers/UrunDetayController.cs
@@ -0,0 +1,24 @@
+﻿using MvcOnlineTicariOtomasyon.Models.Siniflar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class UrunDetayController : Controller
+    {
+        Context c = new Context();
+
+        // GET: UrunDetay
+        public ActionResult Index()
+        {
+            Class1 cs = new Class1();
+            cs.Uruns = c.Uruns.Where(x => x.UrunID == 1).ToList();
+            cs.Detays = c.Detays.Where(y => y.DetayID == 1).ToList();
+           // var values = c.Uruns.Where(x => x.UrunID == 1).ToList();
+            return View(cs);
+        }
+    }
+}
---
+++ MvcOnlineTicariOtomasyon/Models/Siniflar/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyon.Models.Siniflar
+{
+    public class Class1
+    {
+        public IEnumerable<Urun> Uruns { get; set; }
+        public IEnumerable<Detay> Detays { get; set; }
+    }
+}
