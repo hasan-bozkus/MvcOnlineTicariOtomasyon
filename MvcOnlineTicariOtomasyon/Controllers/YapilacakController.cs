bd01ac37c5cb5f6fdc99647615f447b9323fcb34
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2024-06-01 22:49:04 +0300

    MvcOnlineTicariOtomasyon Projesi
    Bölüm 17 - To Do List Bölümü:
    Yapılacaklar Sınıfı Code First Migration Eklenmesi,
    Temanın Entegre Edilmesi,
    To Do List Menüsü,
     To Do List Kartları,
    Distinct Şehir,
    To Do List Verileri

---
+++ MvcOnlineTicariOtomasyon/Controllers/YapilacakController.cs
@@ -0,0 +1,33 @@
+﻿using MvcOnlineTicariOtomasyon.Models.Siniflar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class YapilacakController : Controller
+    {
+        Context context = new Context();
+
+
+        // GET: Yapilacak
+        public ActionResult Index()
+        {
+            var toplamCari = context.Carilers.Count().ToString();
+            var urunler = context.Uruns.Count().ToString();
+            var kategoriler = context.Kategoris.Count().ToString();
+            var carilerSehir = (from x in context.Carilers select x.CariSehir).Distinct().Count().ToString();
+
+            ViewBag.toplamCari = toplamCari;
+            ViewBag.urunler = urunler;
+            ViewBag.kategoriler = kategoriler;
+            ViewBag.carilerSehir = carilerSehir;
+
+
+            var yapilacaklar = context.Yapilacaks.ToList();
+            return View(yapilacaklar);
+        }
+    }
+}
