7df9f9f73f21e8b3017e0ddcae694e2e1ff23619
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2024-06-01 22:59:58 +0300

    MvcOnlineTicariOtomasyon Projesi
    Bölüm 18 - Ekstra Yapılar:
    Display Name Kullanımı 1,
    Tabloda Görselleri Getirme

--- MvcOnlineTicariOtomasyon/Models/Siniflar/Personel.cs
+++ MvcOnlineTicariOtomasyon/Models/Siniflar/Personel.cs
@@ -14,14 +14,17 @@ namespace MvcOnlineTicariOtomasyon.Models.Siniflar
 
         [Column(TypeName = "Varchar")]
         [StringLength(30)]
+        [Display(Name = "Personel Ad")]
         public string PersonelAd { get; set; }
 
         [Column(TypeName = "Varchar")]
         [StringLength(30)]
+        [Display(Name = "Personel Soyad")]
         public string PersonelSoyad { get; set; }
 
         [Column(TypeName = "Varchar")]
         [StringLength(250)]
+        [Display(Name = "Görsel")]
         public string PersonelGorsel { get; set; }
         public ICollection<SatisHareket> SatisHarekets { get; set; }
         public int DepartmanID { get; set; }
