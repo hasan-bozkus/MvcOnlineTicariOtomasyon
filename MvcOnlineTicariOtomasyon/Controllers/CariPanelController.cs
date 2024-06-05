a8bb82db8921e772a0558e37f0ac4bf5db5c5851
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2024-06-05 10:55:39 +0300

    MvcOnlineTicariOtomasyon Projesi
    Bölüm 19 - Login İşlemi:
    Tema Linki,
    Login Formu Entegre Etme,
    Ana Formu Türkçeleştirme,
    Popuplarda Düzenlemeler,
    Son Düzenlemeler,
    Kayıt İşlemini Partiala Taşıma,
    Migrations Update Database Cari Şifre,
    Yeni Cari Kaydı,
    Cari Panel Tema Linki,
    Cari Profilim Paneli Tanıtımı,
    Cari Panelim Controller Tanımlaması,
    Panelin Layout Kısmına Entegre Edilmesi,
    Authentication,
    Login İşlemi,
    Session ile Mail Taşıma,
    Session'a Ait Cari Bilgilerini Listeleme,
    Carinin Satış Geçmişini Görmesi,
    Sessionda Fiyat ve Tarih Bilgisi Getirme

---
+++ MvcOnlineTicariOtomasyon/Controllers/CariPanelController.cs
@@ -0,0 +1,32 @@
+﻿using MvcOnlineTicariOtomasyon.Models.Siniflar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class CariPanelController : Controller
+    {
+        Context c = new Context();
+
+        [Authorize]
+        public ActionResult Index()
+        {
+            var mail = (string)Session["CariMail"];
+            var degerler = c.Carilers.FirstOrDefault(x => x.CariMail == mail);
+            ViewBag.mail = mail;
+            return View(degerler);
+        }
+
+        public ActionResult Siparislerim()
+        {
+            var mail = (string)Session["CariMail"];
+            var id = c.Carilers.Where(x => x.CariMail == mail.ToString()).Select(y => y.CariId).FirstOrDefault();
+            var degerler = c.SatisHarekets.Where(x => x.CariID == id).ToList();
+
+            return View(degerler);
+        }
+    }
+}
---
+++ MvcOnlineTicariOtomasyon/Controllers/LoginController.cs
@@ -0,0 +1,68 @@
+﻿using MvcOnlineTicariOtomasyon.Models.Siniflar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class LoginController : Controller
+    {
+        Context c = new Context();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public PartialViewResult LoginRegister()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public ActionResult LoginRegister(Cariler cariler)
+        {
+            c.Carilers.Add(cariler);
+            c.SaveChanges();
+            return PartialView();
+        }
+
+        [HttpGet]
+        public PartialViewResult LoginCariLogin()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public ActionResult LoginCariLogin(Cariler cariler)
+        {
+            var bilgiler = c.Carilers.FirstOrDefault(x=> x.CariMail == cariler.CariMail && x.CariSifre == cariler.CariSifre);
+            if (bilgiler != null)
+            {
+                FormsAuthentication.SetAuthCookie(bilgiler.CariMail, false);
+                Session["CariMail"] = bilgiler.CariMail.ToString();
+                return RedirectToAction("Index", "CariPanel");
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
+        [HttpGet]
+        public PartialViewResult LoginPersonelLogin()
+        {
+            return PartialView();
+        }
+
+        //[HttpPost]
+        //public ActionResult LoginPersonelLogin()
+        //{
+        //    return PartialView();
+        //}
+    }
+}
