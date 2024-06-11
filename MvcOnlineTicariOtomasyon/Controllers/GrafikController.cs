19560c4fea99b6f8002f1e4b918353df85e2820d
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2024-06-11 21:53:38 +0300

    MvcOnlineTicariOtomasyon Projesi
    Bölüm 27 - Yeni Bölüm Neler Yapacağız Ders Talepleri:
    Sona Doğru Gelirken Ders Talepleri
    
    Bölüm 28 - Grafikler:
    View Üzerinde Chart Kullanımı,
    Chart Kullanımı,
    Controller Tarafında Grafik Oluşturma,
    Grafilklere Veri Tabanından Veri Çekme,
    Google Chartlar Controller,
    Google Chart View Kodları,
    Google Chartlar View,
    Pie Chart Line Chart,
    Google Chartlara Veri Tabanından Veri Çekme,
    Google Chartlar Layout Üzerinde Ayarlanması,
    Line Chart İçin Sayfa Oluşturma,
    Column Chart İçin Sayfa Oluşturma

---
+++ MvcOnlineTicariOtomasyon/Controllers/GrafikController.cs
@@ -0,0 +1,120 @@
+﻿using MvcOnlineTicariOtomasyon.Models.Siniflar;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class GrafikController : Controller
+    {
+        Context c = new Context();
+
+        // GET: Grafik
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Index2()
+        {
+            var grafikCiz = new Chart(width: 600, height: 600);
+            grafikCiz.AddTitle("Kategori - Ürün Stok Sayısı").AddLegend("Stok").AddSeries("Değerler", xValue: new[] { "Mobilya", "Ofis Eşyaları", "Bilgisayar" }, yValues: new[] { 85, 66, 98 }).Write();
+            return File(grafikCiz.ToWebImage().GetBytes(), "Image/jpeg");
+        }
+
+        public ActionResult Index3()
+        {
+            ArrayList xValue = new ArrayList();
+            ArrayList yValue = new ArrayList();
+            var sonuclar = c.Uruns.ToList();
+            sonuclar.ToList().ForEach(x => xValue.Add(x.UrunAd));
+            sonuclar.ToList().ForEach(y => yValue.Add(y.Stok));
+            var grafik = new Chart(width: 800, height: 800)
+                .AddTitle("Stoklar")
+                .AddSeries(chartType: "Pie", name: "Stok", xValue: xValue, yValues: yValue);
+            return File(grafik.ToWebImage().GetBytes(), "Image/jpeg");
+        }
+
+        public ActionResult Index4()
+        {
+            return View();
+        }
+
+        public ActionResult VisualizeUrunResult()
+        {
+            return Json(UrunListesi(), JsonRequestBehavior.AllowGet);
+        }
+
+        public List<Sinif1> UrunListesi()
+        {
+            List<Sinif1> snf = new List<Sinif1>();
+            snf.Add(new Sinif1()
+            {
+                urunad = "Bilgisayar",
+                stok = 120
+            });
+            snf.Add(new Sinif1()
+            {
+                urunad = "Beyaz Eşya",
+                stok = 150
+            });
+            snf.Add(new Sinif1()
+            {
+                urunad = "Mobilya",
+                stok = 70
+            });
+            snf.Add(new Sinif1()
+            {
+                urunad = "Küçük Ev Aletleri",
+                stok = 90
+            });
+            snf.Add(new Sinif1()
+            {   
+                urunad = "Mobil Cihazları",
+                stok = 80
+            });
+
+            return snf;
+        }
+
+        public ActionResult VisualizeUrunResult2()
+        {
+            return Json(UrunListesi2(), JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Index5()
+        {
+            return View();
+        }
+
+        public List<Sinif2> UrunListesi2()
+        {
+            List<Sinif2> snf = new List<Sinif2>();
+            using(var context = new Context())
+            {
+                snf = c.Uruns.Select(x => new Sinif2
+                {
+                    urn = x.UrunAd,
+                    stk = x.Stok
+                }).ToList();
+            }
+            return snf;
+        }
+
+        public ActionResult Index6()
+        {
+            return View();
+        }
+
+        public ActionResult Index7()
+        {
+            return View();
+        }
+
+    }
+
+}
